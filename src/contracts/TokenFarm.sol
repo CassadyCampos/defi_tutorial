f18c8b591382edc6f6d8bca4a09fe2f890ea5f18
Cassady <cassady.campos@uleth.ca>
2021-12-26 02:52:31 -0700

    dam

--- src/contracts/TokenFarm.sol
+++ src/contracts/TokenFarm.sol
@@ -1,5 +1,15 @@
-pragma solidity >0.5.2;
+pragma solidity ^0.8.0;
+
+import "./DappToken";
+import "./DaiToken";
 
 contract TokenFarm {
     string public name = "Dapp Token Farm";
+    DappToken public dappToken;
+    DaiToken public daiToken;
+
+    constructor(DappToken _dappToken, DaiToken _daiToken) public {
+        dappToken = _dappToken;
+        daiToken = _daiToken;
+    }
 }
