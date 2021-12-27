108286ec855dcc17efb3df801e13bee5bd57b236
Cassady <cassady.campos@uleth.ca>
2021-12-27 02:29:55 -0700

    staking

--- scripts/issue-token.js
+++ scripts/issue-token.js
@@ -1,9 +1,12 @@
-const TokenFarm = artifacts.require('TokenFarm')
+const DappToken = artifacts.require("DappToken")
+const DaiToken = artifacts.require("DaiToken")
+const TokenFarm = artifacts.require("TokenFarm")
 
-module.exports = async function(callback) {
+module.exports = async function (callback) {
   let tokenFarm = await TokenFarm.deployed()
-  await tokenFarm.issueTokens()
-  // Code goes here...
-  console.log("Tokens issued!")
-  callback()
+  await tokenFarm.issueTokens();
+
+  console.log("Tokens issued!");
+
+  callback();
 }
